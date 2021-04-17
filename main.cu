3601bc59a3a2bd9ecf91b01aede2b77b92b245f7
Riley <rileymshea@gmail.com>
2021-04-17 00:27:50 -0400

    refactored chp3 example
    Signed-off-by: Riley <rileymshea@gmail.com>

--- main.cu
+++ main.cu
@@ -1,21 +1,29 @@
 #include "book.h"
 
 /// Simple Cuda addition function
-__global__ void add(int numberA, int numberB, int *sum_result_ptr){
+__global__ void add(int numberA, int numberB, int *sum_result_ptr) {
     *sum_result_ptr = numberA + numberB;
 }
 
 int main() {
     int host_value;  // "host" means the regular cpu/memory
     int *cuda_gpu_ptr; // "device" mean the CUDA code running on the GPU/MEM
+    int count;
+    cudaDeviceProp device_specs;
+
+    HANDLE_ERROR(cudaGetDeviceCount(&count));
+
+    for (int i = 0; i < count; i++) {
+        HANDLE_ERROR(cudaGetDeviceProperties(&device_specs, i));
+    }
 
 
 
     // allocate memory on the cuda device
-    HANDLE_ERROR(cudaMalloc((void**) &cuda_gpu_ptr, sizeof(int)));
+    HANDLE_ERROR(cudaMalloc((void **) &cuda_gpu_ptr, sizeof(int)));
 
     // perform calculation with CUDA on GPU
-    add<<<1,1>>>(2, 7, cuda_gpu_ptr);
+    add<<<1, 1>>>(2, 7, cuda_gpu_ptr);
 
 
     // Copy result from CUDA memory back to host memory
